9807e676dc3b41273f5b68725ef7a7ad37173a9c
mzntaka0 <takao.mizuno110@gmail.com>
2020-12-31 22:06:19 +0900

    added 3d graph

---
+++ src/lib/services/index.tsx
@@ -0,0 +1,26 @@
+import axiosbase from 'axios'
+
+
+export const api = axiosbase.create({
+  baseURL: 'http://localhost:3000',  // TODO: move this info to .env
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  responseType: 'json',
+  xsrfHeaderName: "X-CSRF-Token",
+  withCredentials: true,
+})
+
+
+export const fetcher = async (endPoint, options = {}) => {
+  const result = await api.get(
+    endPoint,
+    options
+  )
+    .then(res => {
+       if (!(res.status === 200)) throw new Error(res.statusText)
+      return res.data
+    })
+  return result
+}
